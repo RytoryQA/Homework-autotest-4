8c946cf7b968dfec040ade54c705a8f73ff72f91
RytoryQA <vikaryzhkina89@gmail.com>
2023-11-09 01:07:27 +0400

    add test dropdown list city

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -8,10 +10,8 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CallbackTest {
 
@@ -19,10 +19,13 @@ public class CallbackTest {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
+    @BeforeEach
+    void openApplication() {
+        open("http://localhost:9999/");
+    }
+
     @Test
     void formSubmissionSuccess() {
-
-        open("http://localhost:9999/");
 
         $("[data-test-id=city] input").setValue("Самара");
         String currentDate = generateDate(4, "dd.MM.yyyy");
@@ -32,11 +35,34 @@ public class CallbackTest {
         $("[data-test-id=phone] input").setValue("+99912345678");
         $("[data-test-id=agreement] .checkbox__box").click();
         $("button.button").click();
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id=notification] .notification__title").shouldHave(exactText("Успешно!"));
-        $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + currentDate));
+        $("[data-test-id=notification] .notification__title")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Успешно!"));
+        $("[data-test-id=notification] .notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + currentDate));
 
+    }
 
+    @Test
+    void testDropdownListCity() {
+
+        $("[data-test-id=city] input").setValue("Мо");
+        $(".menu").shouldBe(visible);
+        $$(".menu-item__control").findBy(text("Москва")).click();
+        String currentDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").sendKeys(currentDate);
+        $("[data-test-id=name] input").setValue("Петр Петров");
+        $("[data-test-id=phone] input").setValue("+99912345679");
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $("button.button").click();
+        $("[data-test-id=notification] .notification__title")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Успешно!"));
+        $("[data-test-id=notification] .notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + currentDate));
     }
 
 }
